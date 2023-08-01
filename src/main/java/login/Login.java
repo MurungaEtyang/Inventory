1c4d823b335451bb7dde756151ab8547d3773b8a
murungaetyang <murungaevans84@gmail.com>
2023-08-01 14:25:39 -0700

    updated

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -7,55 +7,51 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Login {
-    public boolean authenticateUser() {
+    public void authenticateUser() {
         // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter their username and password
-        System.out.println("Enter username: ");
-        String username = scanner.nextLine();
-        System.out.println("Enter password: ");
-        String password = scanner.nextLine();
+        while (true) {
+            // Prompt the user to enter their username and password
+            System.out.println("Enter username: ");
+            String username = scanner.nextLine();
+            System.out.println("Enter password: ");
+            String password = scanner.nextLine();
 
-        // Perform authentication logic here (e.g., check against a database)
+            Connection c;
+            Statement stmt;
+            ResultSet rs;
 
-        Connection c;
-        Statement stmt;
-        ResultSet rs;
+            try {
+                // Load the JDBC driver for SQLite
+                Class.forName("org.sqlite.JDBC");
 
-        try {
-            // Load the JDBC driver for SQLite
-            Class.forName("org.sqlite.JDBC");
+                // Connect to the SQLite database named "Inventory.db"
+                c = DriverManager.getConnection("jdbc:sqlite:Inventory.db");
 
-            // Connect to the SQLite database named "Inventory.db"
-            c = DriverManager.getConnection("jdbc:sqlite:Inventory.db");
+                // Create a statement for executing SQL queries
+                stmt = c.createStatement();
 
-            // Create a statement for executing SQL queries
-            stmt = c.createStatement();
+                // Execute a SELECT query to retrieve data from the "USERS" table
+                String query = "SELECT * FROM USERS WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "'";
+                rs = stmt.executeQuery(query);
 
-            // Execute a SELECT query to retrieve data from the "USERS" table
-            String query = "SELECT * FROM USERS WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "'";
-            rs = stmt.executeQuery(query);
+                // Check if the result set has any rows
+                if (rs.next()) {
+                    System.out.println("Hello "+ username + "! You have successfully logged in! Let's do a bit of admin stuff.");
+                    break;
+                } else {
+                    System.out.println("Invalid username or password. Please try again.");
+                }
 
-            // Check if the result set has any rows
-            if (rs.next()) {
-                System.out.println("Login successful!");
-                return true;
-            } else {
-                System.out.println("Invalid username or password. Please try again.");
                 rs.close();
                 stmt.close();
                 c.close();
-                return false;
+                // Close the result set, statement, and the connection to the database
+            } catch (Exception e) {
+                // If any exception occurs, print the error message and continue the loop
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
             }
-
-            // Close the result set, statement, and the connection to the database
-        } catch (Exception e) {
-            // If any exception occurs, print the error message and exit the program
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
         }
-
-        return false;
     }
 }
